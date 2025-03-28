973f1f97a83079c9534aabb46e6ef6241649ccbd
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2025-03-28 21:07:15 +0300

    add correct test group

--- tests/parser_test.cpp
+++ tests/parser_test.cpp
@@ -3,23 +3,38 @@
 
 using namespace testing;
 
-// mожно использовать параметры в тестах так:
+// можно использовать параметры в тестах так:
 
-using ParserExceptionParamTestSuite = TestWithParam<std::string>;
+using InvalidParserExceptionParamTestSuite = TestWithParam<std::string>;
 
-TEST_P(ParserExceptionParamTestSuite, Sample) {
+TEST_P(InvalidParserExceptionParamTestSuite, Sample) {
     auto code = GetParam();
 
     ASSERT_ANY_THROW(parse(code));
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    SampleGroup,
-    ParserExceptionParamTestSuite,
+    InvalidGroup,
+    InvalidParserExceptionParamTestSuite,
     Values(
         "fn main() {", // missed }
         "fn main() {}$", // extra $
         "fn main() { int x = 5 }" // missed ;
         // etc
+    )
+);
+
+using CorrectParserExceptionParamTestSuite = TestWithParam<std::string>;
+
+TEST_P(CorrectParserExceptionParamTestSuite, CorrectSample) {
+    auto code = GetParam();
+    ASSERT_NO_THROW(parse(code));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    CorrectGroup,
+    CorrectParserExceptionParamTestSuite,
+    Values(
+        "fn main() {}"
     )
 );
